acb614abcbc504c90be5c3c9c57b27186d5efdd3
Song <songxiaoliang@users.noreply.github.com>
2017-03-01 15:31:25 +0800

    Update index.ios.js

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,9 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
+ * ComicBook React Native App
+ * Songlcy create by 2017-01-10
  * @flow
  */
+import {AppRegistry} from 'react-native';
+import Root from './app/root';
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-export default class ComicBook extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('ComicBook', () => ComicBook);
+AppRegistry.registerComponent('ComicBook', () => Root);
